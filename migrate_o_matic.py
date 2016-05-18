482f4752aa3849954b0705b65bde821caf4e4cd2
Alex Funk <lexotron314@gmail.com>
2016-05-18 15:57:08 -0500

    added no-db flag

--- migrate_o_matic.py
+++ migrate_o_matic.py
@@ -4,6 +4,7 @@ import os
 import getpass
 import re
 import shlex
+import xml.dom.minidom
 
 DOCUMENT_ROOT = '/var/www/vhosts/'
 DATABASE_REFS = 'wp-config.php|etc/local.xml$'
@@ -13,7 +14,7 @@ parser.add_argument('site', help='the site to be migrated')
 parser.add_argument('destination', help='where to move the site')
 
 # parser.add_argument('-v', '--verbose', help='explain what you are doing', action='store_true')
-# parser.add_argument('--no-db', help='skip the database migration', action='store_true')
+parser.add_argument('--no-db', help='skip the database migration', action='store_true')
 parser.add_argument('-sdn', '--source-db-name', help='what the database is currently named', required=True)
 parser.add_argument('-ddn', '--dest-db-name', help='what the database should be named, defaults to source-db-name')
 parser.add_argument('-sdu', '--source-db-user',
@@ -27,7 +28,7 @@ parser.add_argument('-ddp', '--dest-db-pass', nargs='?',
 parser.add_argument('-sdh', '--source-db-host', help='where the database currently resides', required=True)
 parser.add_argument('-ddh', '--dest-db-host', help='where the database should go', default='aws-db1.firstscribe.com')
 parser.add_argument('-dsu', '--dest-sftp-user', help='the username for the customer SFTP account', required=True)
-parser.add_argument('-dsp', '--dest-sftp-pass', help='the password for the customer SFTP account', nargs='?', const='prompt')  # I can't figure out how to do anythign with this...
+#parser.add_argument('-dsp', '--dest-sftp-pass', help='the password for the customer SFTP account', nargs='?', const='prompt')  # I can't figure out how to do anything with this...
 args = parser.parse_args()
 
 # Populate the rest of the arguments
@@ -44,8 +45,8 @@ if args.dest_db_pass is None:
     args.dest_db_pass = args.source_db_pass
 elif args.dest_db_pass is 'prompt':
     args.dest_db_pass = getpass.getpass(prompt='Please enter the destination database password: ')
-if args.dest_sftp_pass is 'prompt':
-    args.dest_sftp_pass = getpass.getpass(prompt='Please enter the password for the customer SFTP account: ')
+#if args.dest_sftp_pass is 'prompt':
+#    args.dest_sftp_pass = getpass.getpass(prompt='Please enter the password for the customer SFTP account: ')
 
 # Shorthands for me
 
@@ -67,8 +68,11 @@ def get_folder_size(folder):
             total_size += get_folder_size(itempath)
     return total_size
 
-# Verify ssh-agent is running
-step_placeholder('verify your ssh agent')
+def magento_cred_parse(path):
+    xmldoc = xml.dom.minidom.parse(path)
+    config = xmldoc.documentElement
+    conn = root.find('.//connection')
+
 
 # Verify DNS abilities
 step_placeholder('verify that we can alter DNS')
@@ -85,74 +89,76 @@ step_placeholder('copy the SSL certificates')
 # Copy any special hosting settings/php settings
 step_placeholder('verify the PHP and hosting settings')
 
-# Find database references.  I like grep -R '\.firstscribe\.com'
-db_ref_regexr = re.compile(DATABASE_REFS)
+if not args.no_db:
+    # Find database references.  I like grep -R '\.firstscribe\.com'
+    db_ref_regexr = re.compile(DATABASE_REFS)
 
-possible_db_refs = []
+    possible_db_refs = []
 
-for root, dirs, files in os.walk(site_httpdocs):
-    for name in files:
-        full_path=os.path.join(root, name)
-        if db_ref_regexr.search(full_path):
-            possible_db_refs.append(full_path)
+    for root, dirs, files in os.walk(site_httpdocs):
+        for name in files:
+            full_path=os.path.join(root, name)
+            if db_ref_regexr.search(full_path):
+                possible_db_refs.append(full_path)
 
-print('I see possible database references in:')
-for item in possible_db_refs:
-    print(item)
+    print('I see possible database references in:')
+    for item in possible_db_refs:
+        print(item)
 
-print('Does that look right?')
-input('Press enter to continue.')
+    print('Does that look right?')
+    input('Press enter to continue.')
 
-# Make database (through plesk, to get ref)
-step_placeholder('create the database {0}'.format(args.dest_db_name))
+    # Make database (through plesk, to get ref)
+    step_placeholder('create the database {0}'.format(args.dest_db_name))
 
-# Transfer the Database
+    # Transfer the Database
 
-# I bit off more than I can chew...
+    # I bit off more than I can chew...
 
-# mysqldump_proc = subprocess.Popen(
-#     ('mysqldump', '-u', args.source_db_user, '-h', args.source_db_host, '-p', args.source_db_pass, args.source_db_name),
-#     stdout=subprocess.PIPE)
-# sed_proc = subprocess.Popen(('sed', 's/TIME_ZONE=\'+00:00\'/TIME_ZONE=\'+06:00\'/'), stdout=subprocess.PIPE,
-#                             stdin=mysqldump_proc.stdout)
-# mysqldump_proc.stdout.close()
-# pv_proc = subprocess.Popen('pv',
-#                            stdin=sed_proc.stdout, stdout=subprocess.PIPE)
-# sed_proc.stdout.close()
-# xz_proc = subprocess.Popen(('xz', '-c', '-4'), stdout=subprocess.PIPE, stdin=pv_proc.stdout)
-# pv_proc.stdout.close()
-# # noinspection PyPep8
-# ssh_mysql_proc = subprocess.Popen(('ssh', args.destination,
-#                                    '\"xz -d -c | mysql -u ' + args.dest_db_user + ' -p \\\"' + args.dest_db_pass + '\\\" -h ' + args.dest_db_host + ' ' + args.dest_db_name),
-#                                   stdin=xz_proc.stdout)
-# pv_proc.stdout.close()
-#
-# ssh_mysql_proc.communicate()
-# ssh_mysql_proc.wait()
+    # mysqldump_proc = subprocess.Popen(
+    #     ('mysqldump', '-u', args.source_db_user, '-h', args.source_db_host, '-p', args.source_db_pass, args.source_db_name),
+    #     stdout=subprocess.PIPE)
+    # sed_proc = subprocess.Popen(('sed', 's/TIME_ZONE=\'+00:00\'/TIME_ZONE=\'+06:00\'/'), stdout=subprocess.PIPE,
+    #                             stdin=mysqldump_proc.stdout)
+    # mysqldump_proc.stdout.close()
+    # pv_proc = subprocess.Popen('pv',
+    #                            stdin=sed_proc.stdout, stdout=subprocess.PIPE)
+    # sed_proc.stdout.close()
+    # xz_proc = subprocess.Popen(('xz', '-c', '-4'), stdout=subprocess.PIPE, stdin=pv_proc.stdout)
+    # pv_proc.stdout.close()
+    # # noinspection PyPep8
+    # ssh_mysql_proc = subprocess.Popen(('ssh', args.destination,
+    #                                    '\"xz -d -c | mysql -u ' + args.dest_db_user + ' -p \\\"' + args.dest_db_pass + '\\\" -h ' + args.dest_db_host + ' ' + args.dest_db_name),
+    #                                   stdin=xz_proc.stdout)
+    # pv_proc.stdout.close()
+    #
+    # ssh_mysql_proc.communicate()
+    # ssh_mysql_proc.wait()
 
-print('OK, I am going to try to migrate the database now...')
+    print('OK, I am going to try to migrate the database now...')
 
-db_proc = """mysqldump -u{0} -p{1} -h{2} {3} | sed "s/TIME_ZONE='+00:00'/TIME_ZONE='+06:00'/" | pv | xz -c -4 | ssh {4} "xz -d -c | mysql -u{5} -p{6} -h{7} {8}" """.format(
-        args.source_db_user, shlex.quote(args.source_db_pass), args.source_db_host, args.source_db_name,
-        args.destination, args.dest_db_user, shlex.quote(args.dest_db_pass), args.dest_db_host, args.dest_db_name)
-try:
-    subprocess.call(db_proc, shell=True)  # This is the "wrong" way to do it, but I can't get the nested Popen's to work
-except KeyboardInterrupt:
-    exit(0)
+    db_proc = """mysqldump -u{0} -p{1} -h{2} {3} | sed "s/TIME_ZONE='+00:00'/TIME_ZONE='+06:00'/" | pv | xz -c -4 | ssh {4}@{5} "xz -d -c | mysql -u{6} -p{7} -h{8} {9}" """.format(
+            args.source_db_user, shlex.quote(args.source_db_pass), args.source_db_host, args.source_db_name,
+            args.dest_sftp_user, args.destination, args.dest_db_user, shlex.quote(args.dest_db_pass), args.dest_db_host, args.dest_db_name)
+    try:
+        subprocess.call(db_proc, shell=True)  # This is the "wrong" way to do it, but I can't get the nested Popen's to work
+    except KeyboardInterrupt:
+        exit(0)
 
-# Update the DB refs in local.xmls or wp-config.php
-step_placeholder('update database refs')
+    # Update the DB refs in local.xmls or wp-config.php
+    step_placeholder('update database refs')
 
-# Clear magento cache
-step_placeholder('clear the magento cache')
+    # Clear magento cache
+    step_placeholder('clear the magento cache')
 
-# Make sure you didn't break anything
-step_placeholder('test the original site')
+    # Make sure you didn't break anything
+    step_placeholder('test the original site')
 
 # Transfer the site
 
 # subprocess.call(('sudo chmod g+w {0}'.format(site_httpdocs)))
 # subprocess.call(('ssh', '-t', args.destination, 'sudo chmod g+w {0}'.format(site_httpdocs)))
+print('OK, I am going to try to migrate the site now...')
 subprocess.call(('ssh', args.dest_sftp_user + '@' + args.destination, 'rm -rf {0}/*'.format(site_httpdocs)))
 
 # Bit off more than I can chew...
@@ -181,6 +187,9 @@ step_placeholder('test the site in the new location.')
 
 # Update DNS/Switch Nameserver
 step_placeholder('update the real DNS')
+
+# Transfer cron jobs
+step_placeholder('transfer any cron jobs')
 
 # Disable old site next day
 step_placeholder('make a reminder to disable the site the next day')
